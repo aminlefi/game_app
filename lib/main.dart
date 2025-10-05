5066c34891a9a7586f531925c57a2e0dabe43427
aminlefi <100865641+aminlefi@users.noreply.github.com>
2025-10-05 15:37:11 +0100

    comm

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
-import 'screens/game_purchase_screen.dart';
-import 'screens/registration_screen.dart';
-import 'screens/library_screen.dart';
-import 'screens/login_screen.dart';
+
+import 'pages/library/my_games.dart';
+import 'pages/product_details.dart';
+import 'pages/signup.dart';
+import 'pages/signin.dart';
+import 'pages/home/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,19 +15,23 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Game Store',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      initialRoute: '/library',
-      routes: {
-        '/purchase': (context) => const GamePurchaseScreen(),
-        '/register': (context) => RegistrationScreen(),
-        '/library': (context) => const LibraryScreen(),
-        '/login': (context) => const LoginScreen(),
-      },
-    );
+    return const MaterialApp(title: "Workshops 5GamiX", //home: Home()
+        //home: Signup()
+        // home: Signin()
+        home: MyGames()
+        // home: ProductDetails(
+        //     "assets/images/dmc5.jpg",
+        //     "Devil May Cry 5",
+        //     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, "
+        //         "sed do eiusmod tempor incididunt ut "
+        //         "labore et dolore magna aliqua. Ut enim ad minim veniam, "
+        //         "quis nostrud exercitation ullamco laboris nisi ut "
+        //         "aliquip ex ea commodo consequat. Duis aute irure dolor "
+        //         "in reprehenderit in voluptate velit esse cillum dolore "
+        //         "eu fugiat nulla pariatur. Excepteur sint occaecat "
+        //         "cupidatat non proident, sunt in culpa qui officia "
+        //         "deserunt mollit anim id est laborum.",
+        //     200, 3000)
+        );
   }
 }
